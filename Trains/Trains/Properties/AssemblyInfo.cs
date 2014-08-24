571f062875a51bfdbb2f4446eeb991d165bb061b
Israel Hofman <ihofman@stefanini.com>
2014-08-24 16:18:24 -0300

    Info.

--- Trains/Trains/Properties/AssemblyInfo.cs
+++ Trains/Trains/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Trains")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Develop by Israel Hofman")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ICEMicroSystems")]
 [assembly: AssemblyProduct("Trains")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("ICeHofman")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
